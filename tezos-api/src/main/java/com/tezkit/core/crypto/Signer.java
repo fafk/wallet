d49d5e70c600e1852ed0af9a12321bc398e2fbc8
Honza <patomil@gmail.com>
2020-07-31 08:32:30 +0200

    Remove dependency on apache utils

--- tezos-api/src/main/java/com/tezkit/core/crypto/Signer.java
+++ tezos-api/src/main/java/com/tezkit/core/crypto/Signer.java
@@ -3,7 +3,6 @@ package com.tezkit.core.crypto;
 import com.goterl.lazycode.lazysodium.SodiumJava;
 import lombok.Builder;
 import lombok.Data;
-import org.apache.commons.lang3.ArrayUtils;
 
 import java.util.Arrays;
 
@@ -21,9 +20,9 @@ public class Signer {
     /**
      * Sign data with a private key and a water mark to be shipped off to the network.
      *
-     * @param bytes bytes to sign
+     * @param bytes      bytes to sign
      * @param privateKey private key to sign the data with
-     * @param watermark watermark to prefix operations; 0x03 for wallet operations
+     * @param watermark  watermark to prefix operations; 0x03 for wallet operations
      * @return an immutable data structure holding the data, sig and signed data
      */
     public static SignedData sign(byte[] bytes, byte[] privateKey, String watermark) {
@@ -34,11 +33,11 @@ public class Signer {
                 privateKey, edskPrefix.length, privateKey.length);
 
         // Then we create a work array and check if the watermark parameter has been passed.
-        byte[] workBytes = ArrayUtils.addAll(bytes);
+        byte[] workBytes = addAll(bytes);
 
         if (watermark != null) {
             byte[] wmBytes = HEX.decode(watermark);
-            workBytes = ArrayUtils.addAll(wmBytes, workBytes);
+            workBytes = addAll(wmBytes, workBytes);
         }
 
         // Hash (watermark + bytes) and sign the hash.
@@ -50,7 +49,7 @@ public class Signer {
         // Crate edsig by encoding (edsig_prefix + sig).
         // `sbytes` will be the concatenation of bytes (in hex) + sig (in hex).
         byte[] edsigPrefixedSig;
-        edsigPrefixedSig = ArrayUtils.addAll(edsigPrefix, sig);
+        edsigPrefixedSig = addAll(edsigPrefix, sig);
         String sbytes = HEX.encode(bytes) + HEX.encode(sig);
 
         return SignedData.builder()
@@ -59,6 +58,23 @@ public class Signer {
                 .edsig(TezosBase58.encode(edsigPrefixedSig))
                 .sbytes(sbytes)
                 .build();
+    }
+
+    private static byte[] addAll(byte[] array1, byte... array2) {
+        if (array1 == null) {
+            return clone(array2);
+        } else if (array2 == null) {
+            return clone(array1);
+        } else {
+            byte[] joinedArray = new byte[array1.length + array2.length];
+            System.arraycopy(array1, 0, joinedArray, 0, array1.length);
+            System.arraycopy(array2, 0, joinedArray, array1.length, array2.length);
+            return joinedArray;
+        }
+    }
+
+    private static byte[] clone(byte[] array) {
+        return array == null ? null : array.clone();
     }
 
     @Data
